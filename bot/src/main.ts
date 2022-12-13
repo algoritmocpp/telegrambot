8491ca030b17e7954d9a9258169fd65c827aca3f
Pedro Henrique Dias Sousa <xgpkey@gmail.com>
2022-12-13 20:31:39 -0300

    Adaptando main.ts

--- bot/src/main.ts
+++ bot/src/main.ts
@@ -7,18 +7,7 @@ dotenv.config();
 const bot: Telegraf<Context<Update>> = new Telegraf(process.env.TOKEN as string);
 
 bot.start((ctx) => {
-  ctx.reply('Hello ' + ctx.from.first_name + '!');
-});
-bot.help((ctx) => {
-  ctx.reply('Send /start to receive a greeting');
-  ctx.reply('Send /keyboard to receive a message with a keyboard');
-  ctx.reply('Send /quit to stop the bot');
-});
-bot.command('quit', (ctx) => {
-  // Explicit usage
-  ctx.telegram.leaveChat(ctx.message.chat.id);
-// Context shortcut
-  ctx.leaveChat();
+  ctx.reply('Opa ' + ctx.from.first_name + '!');
 });
 
 bot.launch();
