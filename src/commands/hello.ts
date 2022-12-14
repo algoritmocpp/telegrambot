66bbc8dc0e50422f75ed62ab76e35efffb843698
Pedro Henrique Dias Sousa <xgpkey@gmail.com>
2022-12-14 17:34:51 -0300

    Testando update via script

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,5 +1,5 @@
 import { Context } from "telegraf";
 
 export function hello (ctx: Context) {
-  ctx.reply('Eai, tudo bem ' + ctx.from?.first_name + '?');
+  ctx.reply('Eai, tudo bem ' + ctx.from?.first_name + '? Agora posso ser atualizado via script!');
 }
